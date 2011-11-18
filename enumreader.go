cf96fd3eab74ef80e0cb2aa9796c9c902f44ff12
Christoph Schunk <schunk.christoph@googlemail.com>
2011-11-19 00:06:44 +0100

    enumreader refactored

--- enumreader.go
+++ enumreader.go
@@ -6,11 +6,12 @@ import (
 	"io"
 	"bufio"
 	"regexp"
+	"strings"
 )
 
 // TODO: better regexps
 var (
-	enumEmptyLineRE  = regexp.MustCompile("^[ \\t]*(#.*)?\\n")
+	enumEmptyLineRE  = regexp.MustCompile("^[ \\t]*(#.*)?$")
 	enumCategoryRE   = regexp.MustCompile("^[ \\t]*([_0-9A-Za-z]+)[ \\t]+enum:")
 	enumRE           = regexp.MustCompile("^[ \\t]*([_0-9A-Za-z]+)[ \\t]*=[ \\t]*([^ \\t#\\n]*)")
 	enumPassthruRE   = regexp.MustCompile("^[ \\t]*passthru:")
@@ -30,39 +31,39 @@ func ReadEnumsFromFile(name string) (EnumCategories, os.Error) {
 
 func ReadEnums(r io.Reader) (EnumCategories, os.Error) {
 	categories := make(EnumCategories)
+	deferredUseEnums := make(map[string]map[string]string)
+	currentCategory := ""
 	br := bufio.NewReader(r)
 
-	currentCategory := ""
-	deferredUseEnums := make(map[string]map[string]string)
-
-	for line, rerr := br.ReadString('\n'); rerr == nil || rerr == os.EOF; line, rerr = br.ReadString('\n') {
-		if rerr == os.EOF {
-			line += "\n"
-		}
-		//fmt.Printf(line)
-		if !enumEmptyLineRE.MatchString(line) && !enumPassthruRE.MatchString(line) {
-			if category := enumCategoryRE.FindStringSubmatch(line); category != nil {
-				//fmt.Printf("%v\n", category[1])
-				currentCategory = category[1]
-				categories[currentCategory] = make(Enums)
-			} else if enum := enumRE.FindStringSubmatch(line); enum != nil {
-				//fmt.Printf("%v %v\n", enum[1], enum[2])
-				categories[currentCategory][enum[1]] = Enum{enum[1], enum[2]}
-			} else if use := enumUseRE.FindStringSubmatch(line); use != nil {
-				//fmt.Printf("%v %v\n", use[1], use[2])
-				if deferredUseEnums[currentCategory] == nil {
-					deferredUseEnums[currentCategory] = make(map[string]string)
-				}
-				deferredUseEnums[currentCategory][use[2]] = use[1]
-			} else {
-				//return os.NewError("Unable to parse line: '" + line + "'")
-				fmt.Fprintf(os.Stderr, "Unable to parse line: "+line)
-			}
-		}
-		if rerr == os.EOF {
+	for {
+		line, err := br.ReadString('\n')
+		if err == os.EOF {
 			break
+		} else if err != nil {
+			return nil, err
+		}
+		line = strings.TrimRight(line, "\n")
+
+		if enumEmptyLineRE.MatchString(line) || enumPassthruRE.MatchString(line) {
+			// skip
+		} else if category := enumCategoryRE.FindStringSubmatch(line); category != nil {
+			//fmt.Printf("%v\n", category[1])
+			currentCategory = category[1]
+			categories[currentCategory] = make(Enums)
+		} else if enum := enumRE.FindStringSubmatch(line); enum != nil {
+			//fmt.Printf("%v %v\n", enum[1], enum[2])
+			categories[currentCategory][enum[1]] = Enum{enum[1], enum[2]}
+		} else if use := enumUseRE.FindStringSubmatch(line); use != nil {
+			//fmt.Printf("%v %v\n", use[1], use[2])
+			if deferredUseEnums[currentCategory] == nil {
+				deferredUseEnums[currentCategory] = make(map[string]string)
+			}
+			deferredUseEnums[currentCategory][use[2]] = use[1]
+		} else {
+			fmt.Fprintf(os.Stderr, "WARNING: Unable to parse line: "+line)
 		}
 	}
+
 	for category, enums := range deferredUseEnums {
 		for name, referencedCategory := range enums {
 			if dereference, ok := categories[referencedCategory][name]; ok {
@@ -74,5 +75,6 @@ func ReadEnums(r io.Reader) (EnumCategories, os.Error) {
 			}
 		}
 	}
+
 	return categories, nil
 }
