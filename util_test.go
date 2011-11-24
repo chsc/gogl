aad4d3ef9ca9a719f75b6ebb471e1114973f2918
Christoph Schunk <schunk.christoph@googlemail.com>
2011-11-24 22:21:22 +0100

    Unit test for GoName() in util.go

--- util_test.go
+++ util_test.go
@@ -10,11 +10,25 @@ type testCategories struct {
 	out ParsedCategoryString
 }
 
+type testGoName struct {
+	in  string
+	out string
+}
+
 var allTestCategories = []testCategories{
 	{"GL_VERSION_1_4", ParsedCategoryString{CategoryVersion, Version{1, 4}, "", ""}},
 	{"GL_VERSION_2_0_DEPRECATED", ParsedCategoryString{CategoryDeprecatedVersion, Version{2, 0}, "", ""}},
 	{"GL_ARB_multisample", ParsedCategoryString{CategoryExtension, Version{0, 0}, "ARB", "multisample"}},
 	{"GL_EXT_12345678", ParsedCategoryString{CategoryExtension, Version{0, 0}, "EXT", "12345678"}},
+}
+
+
+var allTestGoName = []testGoName{
+	{"", ""},
+	{"_", ""},
+	{"ARB_multisample", "ARBMultisample"},
+	{"vertex_array_object", "VertexArrayObject"},
+	{"a_b_c_", "ABC"},
 }
 
 func TestParsedCategoryString(t *testing.T) {
@@ -27,6 +41,15 @@ func TestParsedCategoryString(t *testing.T) {
 		t.Logf("%v", te)
 		if !reflect.DeepEqual(pc, te.out) {
 			t.Errorf("not equal")
+		}
+	}
+}
+
+func TestGoName(t *testing.T) {
+	for i := range allTestGoName {
+		te := &allTestGoName[i]
+		if GoName(te.in) != te.out {
+			t.Errorf("String conversion failed: %v -> %v", te.in, te.out)
 		}
 	}
 }
