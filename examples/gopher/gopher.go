45997115a2413874d8692ce58ac419b92363dd44
christoph <christoph@orion.(none)>
2012-01-05 22:24:31 +0100

    Spinning gopher example added.

---
+++ examples/gopher/gopher.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	gl "github.com/chsc/gogl/gl21"
+	"github.com/jteeuwen/glfw"
+	"image"
+	"image/png"
+	"os"
+)
+
+const (
+	Title  = "Spinning Gopher"
+	Width  = 640
+	Height = 480
+)
+
+var (
+	texture     gl.Uint
+	rotx, roty  gl.Float
+	ambient     []gl.Float  = []gl.Float{0.5, 0.5, 0.5, 1}
+	diffuse     []gl.Float  = []gl.Float{1, 1, 1, 1}
+	lightpos    []gl.Float  = []gl.Float{-5, 5, 10, 0}
+)
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "glfw: %s\n", err)
+		return
+	}
+	defer glfw.Terminate()
+
+	glfw.OpenWindowHint(glfw.WindowNoResize, 1)
+
+	if err := glfw.OpenWindow(Width, Height, 0, 0, 0, 0, 0, 0, glfw.Windowed); err != nil {
+		fmt.Fprintf(os.Stderr, "glfw: %s\n", err)
+		return
+	}
+	defer glfw.CloseWindow()
+
+	glfw.SetSwapInterval(1)
+	glfw.SetWindowTitle(Title)
+
+	if err := gl.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "gl: %s\n", err)
+	}
+
+	if err := initScene(); err != nil {
+		fmt.Fprintf(os.Stderr, "init: %s\n", err)
+		return
+	}
+	defer destroyScene()
+
+	for glfw.WindowParam(glfw.Opened) == 1 {
+		drawScene()
+		glfw.SwapBuffers()
+	}
+}
+
+func createTexture(textureName string) (textureId gl.Uint, err error) {
+	f, err := os.Open(textureName)
+	if err != nil {
+		return 0, err
+	}
+	img, err := png.Decode(f)
+	if err != nil {
+		return 0, err
+	}
+	rgbaImg, ok := img.(*image.NRGBA)
+	if !ok {
+		return 0, errors.New("texture must be an NRGBA image")
+	}
+
+	gl.GenTextures(1, &textureId)
+	gl.BindTexture(gl.TEXTURE_2D, textureId)
+	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+
+	// flip image: first pixel is lower left corner
+	imgWidth, imgHeight := img.Bounds().Dx(), img.Bounds().Dy()
+	data := make([]byte, imgWidth * imgHeight * 4)
+	lineLen := imgWidth * 4
+	dest := len(data)-lineLen
+	for src := 0; src < len(rgbaImg.Pix); src+=rgbaImg.Stride {
+		copy(data[dest:dest+lineLen], rgbaImg.Pix[src:src+rgbaImg.Stride])
+		dest-=lineLen
+	}
+	gl.TexImage2D(gl.TEXTURE_2D, 0, 4, gl.Sizei(imgWidth), gl.Sizei(imgHeight), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Pointer(&data[0]))
+
+	return textureId, nil
+}
+
+func initScene() (err error) {
+	gl.Enable(gl.TEXTURE_2D)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.Enable(gl.LIGHTING)
+
+	gl.ClearColor(0.5, 0.5, 0.5, 0.0)
+	gl.ClearDepth(1)
+	gl.DepthFunc(gl.LEQUAL)
+
+	gl.Lightfv(gl.LIGHT0, gl.AMBIENT, &ambient[0])
+	gl.Lightfv(gl.LIGHT0, gl.DIFFUSE, &diffuse[0])
+	gl.Lightfv(gl.LIGHT0, gl.POSITION, &lightpos[0])
+	gl.Enable(gl.LIGHT0)
+
+	gl.Viewport(0, 0, Width, Height)
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadIdentity()
+	gl.Frustum(-1, 1, -1, 1, 1.0, 10.0)
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.LoadIdentity()
+
+	texture, err = createTexture("gopher.png")
+	return
+}
+
+func destroyScene() {
+	gl.DeleteTextures(1, &texture)
+}
+
+func drawScene() {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.LoadIdentity()
+	gl.Translatef(0, 0, -3.0)
+	gl.Rotatef(rotx, 1, 0, 0)
+	gl.Rotatef(roty, 0, 1, 0)
+
+	rotx += 0.5
+	roty += 0.5
+
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+
+	gl.Color4f(1, 1, 1, 1)
+
+	gl.Begin(gl.QUADS)
+
+	gl.Normal3f(0, 0, 1)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(-1, -1, 1)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(1, -1, 1)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(1, 1, 1)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(-1, 1, 1)
+
+	gl.Normal3f(0, 0, -1)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(-1, -1, -1)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(-1, 1, -1)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(1, 1, -1)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(1, -1, -1)
+
+	gl.Normal3f(0, 1, 0)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(-1, 1, -1)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(-1, 1, 1)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(1, 1, 1)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(1, 1, -1)
+
+	gl.Normal3f(0, -1, 0)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(-1, -1, -1)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(1, -1, -1)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(1, -1, 1)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(-1, -1, 1)
+
+	gl.Normal3f(1, 0, 0)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(1, -1, -1)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(1, 1, -1)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(1, 1, 1)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(1, -1, 1)
+
+	gl.Normal3f(-1, 0, 0)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex3f(-1, -1, -1)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex3f(-1, -1, 1)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex3f(-1, 1, 1)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex3f(-1, 1, -1)
+
+	gl.End()
+}
